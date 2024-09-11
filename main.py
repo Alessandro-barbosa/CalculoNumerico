f08f6adfb948e282da7fa0bfd7df293e9b2f0f71
Alessandro barbosa <66150051+Alessandro-barbosa@users.noreply.github.com>
2024-09-11 09:24:15 -0300

    Alterações no limite das matrizes

--- main.py
+++ main.py
@@ -4,22 +4,28 @@ print("")
     4x + 7y + 3z = 19 
     1x + 1y + 2z = 8 
 '''
-# matrizA = [[2, 3, 1], [4, 7, 3], [1, 1, 2]]
-# B = [9, 19, 8]
-matrizA = [[1, 2], [3, -1]]
-B = [5, 4]
+matrizA = [[2, 3, 1], [4, 7, 3], [1, 1, 2]]
+B = [9, 19, 8]
+#matrizA = [[1, 2], [3, -1]]
+#B = [5, 4]
 # x = ['x', 'y']
-a = 0
+#novosValores = (matrizA[i+1][j] - (multiplicadorDeLinha * matrizA[i - 1][j]))
+linha = 0
 b = 0
-pivo = 0
 novaLinha = []
-tamanho = len(matrizA[0])
+pivo = matrizA[linha][b]
+pivo = float(pivo)
 for i in range(len(matrizA)):
-    pivo = matrizA[a][b]
-    if i == 0:
-        multiplicadorDeLinha = (matrizA[a + 1][b] / pivo)
+    if i < len(matrizA[0]) - 1:
+        multiplicadorDeLinha = (matrizA[i + 1][b] / pivo)
     for j in range(len(matrizA)):
-        if i > 0 or i < len(matrizA[0]):
-            novosValores = (matrizA[i+1][j] - (multiplicadorDeLinha * matrizA[i - 1][j]))
-            print(f"novosValores {novosValores}")
+        if i == 0:
+            novaLinha.append(matrizA[i + 1][j] -
+                             (multiplicadorDeLinha * matrizA[i][j]))
+    if i < len(matrizA[0]) - 1:
+        matrizA[linha + 1] = novaLinha
+        linha += 1
+        b += 1
+        pivo = matrizA[linha][b]
+print(novaLinha)
 print(matrizA)
